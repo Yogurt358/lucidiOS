ac62a80ab27d216d19c069d033205a4d649d1bda
Roy Berenstein <mrkrembo99@gmail.com>
2026-01-25 13:42:17 +0000

    adding TSS functions

--- src/gdt_handle.h
+++ src/gdt_handle.h
@@ -3,12 +3,14 @@
 #include <stddef.h>
 #include "limine.h"
 
+#define TSS_ENTRY_SIZE 104
+
 struct tss_entry {
-    uint32_t _reserved0;
+    uint32_t reserved0;
     uint64_t rsp0;
     uint64_t rsp1;
     uint64_t rsp2;
-    uint64_t _reserved1;
+    uint64_t reserved1;
     uint64_t ist1;
     uint64_t ist2;
     uint64_t ist3;
@@ -16,9 +18,21 @@ struct tss_entry {
     uint64_t ist5;
     uint64_t ist6;
     uint64_t ist7;
-    uint64_t _reserved2;
-    uint16_t _reserved3;
+    uint64_t reserved2;
+    uint16_t reserved3;
     uint16_t iopb;
+} __attribute__((packed));
+
+struct tss_descriptor { 
+    uint16_t limit1;
+    uint16_t base1;
+    uint8_t base2;
+    uint8_t access_byte;
+    uint8_t limit2:4;
+    uint8_t flags:4;
+    uint8_t base3;
+    uint32_t base4;
+    uint32_t reserved0;
 } __attribute__((packed));
 
 struct gdtr {
@@ -26,13 +40,15 @@ struct gdtr {
     uint64_t base;
 } __attribute__((packed));
 
+uint16_t tss_selector = 0x28;
+
 typedef struct gdtr gdtr_t;
 typedef struct tss_entry tss_entry_t;
+typedef struct tss_descriptor tss_descriptor_t;
 
 static inline void sgdt(gdtr_t* ptr) {
     asm volatile ("sgdt %0" :"=m"(*ptr)::"memory");
 }
 void init_tss();
-void load_tss();
-void switch_save();
-
+//void load_tss();
+//void switch_save();
--- src/tss_load.c
+++ src/tss_load.c
@@ -3,10 +3,30 @@
 
 volatile uint8_t kernel_stack[8192];
 
-void init_tss() { // gives all the tss entry components their needed values, need to use memset, how?
+void init_tss() {
+
+    //limit is the limit of the TSS_entry
+    //base is the address of the entry
+
+    tss_entry_t entry;
+    memset(&entry,0,TSS_ENTRY_SIZE);
+    entry.rsp0 = (uint64_t)&kernel_stack + sizeof(kernel_stack);
+    entry.iopb = sizeof(tss_entry_t);
+
+
+       
+    tss_descriptor_t descriptor;
+    memset(&descriptor,0,sizeof(tss_descriptor_t));
+    /*descriptor.limit1 = 
+    descriptor.limit2 = 
+    descriptor.base1 = 
+    descriptor.base2 = 
+    descriptor.base3 = 
+    descriptor.base4 = 
+    */descriptor.access_byte = 0b10010001;
+    descriptor.flags = 0b0000;
 
 }
 
 //void load_tss(){}
 //void switch_save(){}
-
