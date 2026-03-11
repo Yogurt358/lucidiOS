2f71a6a26ec00ed403cb30a238b7453eebe34253
Roy Berenstein <mrkrembo99@gmail.com>
2026-03-11 22:58:52 +0000

    changed file arrengments

---
+++ src/apic.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "common.h"
+
+#define IA32_APIC_BASE_MSR 0x1B
+#define IA32_APIC_BASE_MSR_BSP 0x100 
+#define IA32_APIC_BASE_MSR_ENABLE 0x800
+
+//APIC_timer registers
+#define LVT_timer(high)                 (*(volatile uint32_t*)((high) + 0xFEE00320))
+#define Initial_Count_R(high)           (*(volatile uint32_t*)((high) + 0xFEE00380))
+#define Current_Count_R(high)           (*(volatile uint32_t*)((high) + 0xFEE00390))
+#define Divide_Configuration_R(high)    (*(volatile uint32_t*)((high) + 0xFEE003E0))
+#define EOI(high)                       (*(volatile uint32_t*)((high) + 0xFEE000B0))
+
+void init_APIC(uint64_t hhdm_offset);
+void init_APIC_timer(uint64_t hhdm_offset);
