3c0b6136b860447d40cfecf7b5b811eed8b034e9
Roy Berenstein <mrkrembo99@gmail.com>
2026-03-30 00:40:05 +0300

    pmm implementation done

--- src/pmm.c
+++ src/pmm.c
@@ -3,6 +3,38 @@
 static uint64_t* bitmap;
 extern uint64_t g_hhdm_offset;
 
+//------------------------------------PMM------------------------------------------
+void pmm_free_page(uint64_t page_idx) {
+    uint64_t array_index = (page_idx) / 64; // The "Row" (which uint64_t)
+    uint64_t bit_index = page_idx % 64;   // The "Column" (which bit)
+
+    bitmap[array_index] &= ~(1ULL << bit_index);
+}
+
+void pmm_lock_page(uint64_t page_idx) {
+    uint64_t array_index = (page_idx) / 64; // The "Row" (which uint64_t)
+    uint64_t bit_index = page_idx % 64;   // The "Column" (which bit)
+
+    bitmap[array_index] |= (1ULL << bit_index);
+}
+
+
+uint64_t pmm_alloc2(size_t length) { // assuming length is page to bit
+    for(size_t i = 0; i < length/64; i++) {
+        if (bitmap[i] == ~0ULL) continue;
+        for(size_t j = 0; j < 64; j++) {
+            if(!(bitmap[i] & (1ULL << j))) {
+                pmm_lock_page(i*64+j);
+                return (i*64+j)*PAGE_SIZE;
+            }
+            
+        }
+    }
+    return 0;
+}
+//------------------------------------PMM------------------------------------------
+
+//------------------------------------BITMAP------------------------------------------
 bool is_entry_type(size_t type, size_t i, struct limine_memmap_response *map) {
     if (map->entries[i]->type == type) {
             return 1;
@@ -10,27 +42,68 @@ bool is_entry_type(size_t type, size_t i, struct limine_memmap_response *map) {
     return 0;
 }
 
-uint64_t get_bitmap_length(struct limine_memmap_response *map) { // returns length of bitmap
+uint64_t get_bitmap_length(struct limine_memmap_response *map, int data_type) { // returns length of bitmap
     uint64_t max_addr = 0;
+    uint64_t result;
     for(uint64_t i = 0; i<map->entry_count; i++) {
         uint64_t entry_end = map->entries[i]->base + map->entries[i]->length;
         if(entry_end >= max_addr) {
             max_addr = entry_end;
         }
     } // by the end of this loop, max will have the absolute last byte in the RAM. 
-    return (max_addr+4095)/PAGE_SIZE;
+    result = (max_addr+PAGE_SIZE-1)/PAGE_SIZE;
+    return (result+data_type-1)/data_type;
 }
 
-void set_bitmap_pmm(struct limine_memmap_response *map) {
-    size_t bitmap_length = get_bitmap_length(map);
-    bitmap_length = (bitmap_length+63)/64; // the length in uint64_t
-
+void set_useable_ram(struct limine_memmap_response *map) {
     for (uint64_t i = 0; i < map->entry_count; i++) {
-        // Look for a USABLE block that is big enough
-        if (is_entry_type(LIMINE_MEMMAP_USABLE, i, map) && map->entries[i]->length >= (bitmap_length*8)) {
+        if(is_entry_type(LIMINE_MEMMAP_USABLE, i, map)) {
+            // Calculate absolute start and count
+            uint64_t start_page = map->entries[i]->base / PAGE_SIZE;
+            uint64_t page_count = map->entries[i]->length / PAGE_SIZE;
+
+            for(uint64_t j = 0; j < page_count; j++) {
+                // Just pass the absolute page number
+                pmm_free_page(start_page + j);
+            }
+        }
+    }
+}
+
+void protect_bitmap_space(size_t length) { // assuming length is page to qword
+    uintptr_t phys_pos = (uintptr_t)bitmap - g_hhdm_offset;
+    uint64_t pos = (uint64_t)(phys_pos)/PAGE_SIZE;
+    size_t pages_to_lock = (length*8 + PAGE_SIZE - 1) / PAGE_SIZE;
+    // length is already here
+    for(size_t i = 0; i < pages_to_lock; i++) {
+        pmm_lock_page(pos + i);
+    }
+}
+
+void all_bitmap_1(size_t length) {
+    for(size_t i = 0; i < length; i++) bitmap[i] = ~0ULL; // everything is unavailable
+}
+
+void bitmap_pointer(struct limine_memmap_response *map, size_t length) {
+    size_t required_bytes = length * 8; // assuming length is page to qword
+    for(size_t i = 0; i < map->entry_count; i++) {
+        if(is_entry_type(LIMINE_MEMMAP_USABLE, i, map) && map->entries[i]->length >= required_bytes) {
             bitmap = (uint64_t*)(map->entries[i]->base + g_hhdm_offset);
-            for (size_t j = 0; j<bitmap_length; j++) {bitmap[j] = 0xFFFFFFFFFFFFFFFF;}
             break;
         }
     }
 }
+
+void init_bitmap_pmm(struct limine_memmap_response *map) {
+    size_t bitmap_length = get_bitmap_length(map, PAGE_TO_QWORD); // bit map
+    bitmap_pointer(map, bitmap_length);
+    all_bitmap_1(bitmap_length);
+    set_useable_ram(map);
+    protect_bitmap_space(bitmap_length);
+    pmm_lock_page(0); // just for safety and debugging
+
+}
+//------------------------------------BITMAP------------------------------------------
+
+
+
--- src/pmm.h
+++ src/pmm.h
@@ -2,7 +2,20 @@
 #include "common.h"
 
 #define PAGE_SIZE 4096
-
+#define PAGE_TO_BIT 1
+#define PAGE_TO_QWORD 64
+//------------------------------------BITMAP------------------------------------------
 bool is_entry_type(size_t type, size_t i, struct limine_memmap_response *map);
-uint64_t get_bitmap_length(struct limine_memmap_response *map);
-void set_bitmap_pmm(struct limine_memmap_response *map);
+uint64_t get_bitmap_length(struct limine_memmap_response *map, int data_type);
+void init_bitmap_pmm(struct limine_memmap_response *map);
+void set_useable_ram(struct limine_memmap_response *map);
+void all_bitmap_1(size_t length);
+void bitmap_pointer(struct limine_memmap_response *map, size_t length);
+void protect_bitmap_space(size_t length);
+//------------------------------------BITMAP------------------------------------------
+
+//------------------------------------PMM------------------------------------------
+uint64_t pmm_alloc2(size_t length);
+void pmm_free_page(uint64_t page_idx);
+void pmm_lock_page(uint64_t page_idx);
+//------------------------------------PMM------------------------------------------
